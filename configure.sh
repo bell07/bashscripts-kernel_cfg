4fe99fb5ccf1c8a1bf662f844fccb25531e8305c
Alexander Weber <web.alexander@web.de>
2018-09-26 07:11:15 +0200

    create support for settings.txt

--- configure.sh
+++ configure.sh
@@ -1,7 +1,18 @@
 #!/bin/sh
-CFGPATH="$(dirname "$(readlink -f "$0")")"/cfg
-cd /usr/src/linux
 
-echo "Apply config patches from" $CFGPATH
-cat arch/x86/configs/x86_64_defconfig "$CFGPATH"/*.config > .config
+APPL_DIR=$(dirname "$(readlink -f "$0")")
+
+if ! [ -f "$APPL_DIR"/settings.txt ]; then
+	echo "$APPL_DIR"/settings.txt not found. Please use the example file template
+	exit 1
+fi
+
+source "$APPL_DIR"/settings.txt
+
+CFG_PATH="${CFG_PATH:-"$APPL_DIR"/cfg}"
+KERNEL_SOURCE_PATH="${KERNEL_SOURCE_PATH:-/usr/src/linux}"
+
+cd $KERNEL_SOURCE_PATH
+echo "Apply config patches from $CFG_PATH to $KERNEL_SOURCE_PATH"
+cat arch/x86/configs/x86_64_defconfig "$CFG_PATH"/*.config > .config
 make olddefconfig

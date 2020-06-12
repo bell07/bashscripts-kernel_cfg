6929219c861db9395ebf9331008771b052be6f99
Alexander Weber <web.alexander@web.de>
2020-06-12 10:26:06 +0200

    handle auto mount of /boot

--- kernel_build.sh
+++ kernel_build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 APPL_DIR=$(dirname "$(readlink -f "$0")")
 
-
+## Handle Settings
 if [ -n "$1" ]; then
 	SETTINGS_FILE="$(realpath "$1")"
 else
@@ -14,13 +14,20 @@ if ! [ -f "$SETTINGS_FILE" ]; then
 fi
 
 echo "Use settings file $SETTINGS_FILE"
-
 source "$SETTINGS_FILE"
 
 KERNEL_SOURCE_PATH="${KERNEL_SOURCE_PATH:-/usr/src/linux}"
 INSTALL_VERSION="${INSTALL_VERSION:-true}"
 INSTALL_TARGET="${INSTALL_TARGET:-/boot}"
 
+## Handle boot mount
+BOOTMOUNT=0
+if ! mountpoint -q "$INSTALL_TARGET" ; then
+	mount -v "$INSTALL_TARGET" && BOOTMOUNT=1
+fi
+
+
+## Take the installation steps
 cd "$KERNEL_SOURCE_PATH"
 
 make -j6 && make modules_install
@@ -37,4 +44,9 @@ cp -v .config "$INSTALL_TARGET"/config"$APPEND_VERSION"
 if [ -n "$INSTALL_POST_ACTION" ]; then
 	echo ">>> Post installation: $INSTALL_POST_ACTION"
 	$INSTALL_POST_ACTION
+fi
+
+## Umount boot
+if [ "$BOOTMOUNT" == 1 ]; then
+	umount -v "$INSTALL_TARGET"
 fi

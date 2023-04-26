e6004b03963653e5caabc977ef77b670aec299e3
Alexander Weber <web.alexander@web.de>
2023-04-26 08:52:55 +0200

    update distro configs rename distro/gentoo.config -> distro/gentoo-genkernel.config add distro/gentoo-fedora-x86_64.config from sys-kernel/gentoo-kernel source

--- get_distro_defaults.sh
+++ get_distro_defaults.sh
@@ -1,4 +1,5 @@
 APPL_DIR=$(dirname "$(readlink -f "$0")")
 
-wget 'https://gitweb.gentoo.org/proj/genkernel.git/plain/defaults/kernel-generic-config' -O "$APPL_DIR"/distro/gentoo.config
+wget 'https://gitweb.gentoo.org/proj/genkernel.git/plain/defaults/kernel-generic-config' -O "$APPL_DIR"/distro/gentoo-genkernel.config
 wget 'https://raw.githubusercontent.com/archlinux/svntogit-packages/packages/linux/trunk/config' -O "$APPL_DIR"/distro/archlinux.config
+wget 'https://raw.githubusercontent.com/projg2/fedora-kernel-config-for-gentoo/main/kernel-x86_64-fedora.config' -O "$APPL_DIR"/distro/gentoo-fedora-x86_64.config

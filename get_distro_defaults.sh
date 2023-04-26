e954f6d9fb5ae34e31b695ee0e9e5dd6ed756414
Alexander Weber <web.alexander@web.de>
2023-04-26 10:37:23 +0200

    update gentoo-fedora config to latest tag 6.2.6-gentoo

--- get_distro_defaults.sh
+++ get_distro_defaults.sh
@@ -2,4 +2,4 @@ APPL_DIR=$(dirname "$(readlink -f "$0")")
 
 wget 'https://gitweb.gentoo.org/proj/genkernel.git/plain/defaults/kernel-generic-config' -O "$APPL_DIR"/distro/gentoo-genkernel.config
 wget 'https://raw.githubusercontent.com/archlinux/svntogit-packages/packages/linux/trunk/config' -O "$APPL_DIR"/distro/archlinux.config
-wget 'https://raw.githubusercontent.com/projg2/fedora-kernel-config-for-gentoo/main/kernel-x86_64-fedora.config' -O "$APPL_DIR"/distro/gentoo-fedora-x86_64.config
+wget 'https://raw.githubusercontent.com/projg2/fedora-kernel-config-for-gentoo/6.2.6-gentoo/kernel-x86_64-fedora.config' -O "$APPL_DIR"/distro/gentoo-fedora-x86_64.config

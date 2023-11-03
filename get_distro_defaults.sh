e147b40283617e6fd945ca2af915f3ae00335ca3
Alexander Weber <web.alexander@web.de>
2023-11-03 13:37:20 +0100

    update distro defaults

--- get_distro_defaults.sh
+++ get_distro_defaults.sh
@@ -1,8 +1,8 @@
 APPL_DIR=$(dirname "$(readlink -f "$0")")
 
 wget 'https://gitweb.gentoo.org/proj/genkernel.git/plain/defaults/kernel-generic-config' -O "$APPL_DIR"/distro/gentoo-genkernel.config
-wget 'https://raw.githubusercontent.com/archlinux/svntogit-packages/packages/linux/trunk/config' -O "$APPL_DIR"/distro/archlinux.config
-wget 'https://raw.githubusercontent.com/projg2/fedora-kernel-config-for-gentoo/6.4.3-gentoo/kernel-x86_64-fedora.config' -O "$APPL_DIR"/distro/gentoo-fedora-x86_64.config
+wget 'https://gitlab.archlinux.org/archlinux/packaging/packages/linux/-/raw/main/config?inline=false' -O "$APPL_DIR"/distro/archlinux.config
+wget 'https://raw.githubusercontent.com/projg2/fedora-kernel-config-for-gentoo/6.5.8-gentoo/kernel-x86_64-fedora.config' -O "$APPL_DIR"/distro/gentoo-fedora-x86_64.config
 
 cd 3rdparty/gentoo-kernel-config
 git fetch

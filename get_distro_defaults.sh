d2a621b2c1e88b40586f76a44b01d04359d3e2dc
Alexander Weber <web.alexander@web.de>
2025-03-06 12:20:55 +0100

    update distro defaults

--- get_distro_defaults.sh
+++ get_distro_defaults.sh
@@ -1,11 +1,11 @@
 APPL_DIR=$(dirname "$(readlink -f "$0")")
 
-wget 'https://gitlab.com/evlaV/jupiter-PKGBUILD/-/raw/master/linux-neptune-68/config-neptune?inline=false' -O "$APPL_DIR"/cfg/30_enable_steamdeck_device.config.new
+wget 'https://gitlab.com/evlaV/jupiter-PKGBUILD/-/raw/master/linux-neptune-611/config-neptune?inline=false' -O "$APPL_DIR"/cfg/30_enable_steamdeck_device.config.new
 
 
 wget 'https://gitweb.gentoo.org/proj/genkernel.git/plain/defaults/kernel-generic-config' -O "$APPL_DIR"/distro/gentoo-genkernel.config
 wget 'https://gitlab.archlinux.org/archlinux/packaging/packages/linux/-/raw/main/config?inline=false' -O "$APPL_DIR"/distro/archlinux.config
-wget 'https://raw.githubusercontent.com/projg2/fedora-kernel-config-for-gentoo/6.12.8-gentoo/kernel-x86_64-fedora.config' -O "$APPL_DIR"/distro/gentoo-fedora-x86_64.config
+wget 'https://raw.githubusercontent.com/projg2/fedora-kernel-config-for-gentoo/6.13.1-gentoo/kernel-x86_64-fedora.config' -O "$APPL_DIR"/distro/gentoo-fedora-x86_64.config
 
 cd 3rdparty/gentoo-kernel-config
 git fetch

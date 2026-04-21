91716174024a1284a6bce8d9fd6e264bbe2bb284
Alexander Weber <web.alexander@web.de>
2026-04-21 09:24:04 +0200

    update distro defaults

--- get_distro_defaults.sh
+++ get_distro_defaults.sh
@@ -4,7 +4,7 @@ wget 'https://raw.githubusercontent.com/evlaV/jupiter/refs/heads/main/linux-nept
 
 wget 'https://gitweb.gentoo.org/proj/genkernel.git/plain/defaults/kernel-generic-config' -O "$APPL_DIR"/distro/gentoo-genkernel.config
 wget 'https://gitlab.archlinux.org/archlinux/packaging/packages/linux/-/raw/main/config.x86_64?inline=false' -O "$APPL_DIR"/distro/archlinux.config
-wget 'https://raw.githubusercontent.com/projg2/fedora-kernel-config-for-gentoo/6.19.8-gentoo/kernel-x86_64-fedora.config' -O "$APPL_DIR"/distro/gentoo-fedora-x86_64.config
+wget 'https://raw.githubusercontent.com/projg2/fedora-kernel-config-for-gentoo/6.19.11-gentoo/kernel-x86_64-fedora.config' -O "$APPL_DIR"/distro/gentoo-fedora-x86_64.config
 
 cd 3rdparty/gentoo-kernel-config
 git fetch
